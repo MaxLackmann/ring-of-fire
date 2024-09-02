7dde801e2d14862b8e587090ac96006fd026d3cf
MaxLackmann <lackmann_max@hotmail.com>
2024-09-02 16:19:45 +0200

    last update

---
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+
+@Component({
+  selector: 'app-dialog-add-player',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './dialog-add-player.component.html',
+  styleUrl: './dialog-add-player.component.scss',
+})
+export class DialogAddPlayerComponent {
+  name: string = '';
+
+  constructor(public dialogRef: MatDialogRef<DialogAddPlayerComponent>) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onOkClick(): void {
+    if (this.isNameValid()) {
+      this.dialogRef.close(this.name);
+    }
+  }
+
+  isNameValid(): boolean {
+    return this.name.trim().length > 0;
+  }
+}
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { GameInfoComponent } from '../game-info/game-info.component';
+
+@Component({
+  selector: 'app-game',
+  standalone: true,
+  imports: [
+    CommonModule,
+    PlayerComponent,
+    MatButtonModule,
+    MatIconModule,
+    MatDialogModule,
+    GameInfoComponent,
+  ],
+  templateUrl: './game.component.html',
+  styleUrl: './game.component.scss',
+})
+export class GameComponent {
+  pickCardAnimation = false;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor(public dialog: MatDialog) {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop() || '';
+      this.pickCardAnimation = true;
+      console.log('New Card:' + this.currentCard);
+      console.log('Game is:', this.game);
+
+      this.game.currentPlayer++;
+      this.game.currentPlayer =
+        this.game.currentPlayer % this.game.players.length;
+      // %  = Modulus-Operator zum errechnen des aktuellen Spielers 
+      // Modulus ist immer der Restoperator und gibt einen ganzzahligen Rest aus
+
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent, {
+      disableClose: false, // Verhindert das Schließen durch Klicken außerhalb
+    });
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name) {
+        // Überprüfen, ob der Name nicht undefined oder leer ist
+        this.game.players.push(name);
+      }
+    });
+  }
+}
